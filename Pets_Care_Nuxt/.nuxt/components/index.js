13896a007cf4ce85d60b73adad04bba2bf38a486
Jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2020-07-29 22:48:32 -0500

    add components for registerUser and RegisterColl and add component headerregister

--- Pets_Care_Nuxt/.nuxt/components/index.js
+++ Pets_Care_Nuxt/.nuxt/components/index.js
@@ -1,6 +1,7 @@
 export { default as Footer } from '../../components/Footer.vue'
 export { default as Header } from '../../components/Header.vue'
 export { default as HeaderMenu } from '../../components/HeaderMenu.vue'
+export { default as HeaderRegister } from '../../components/HeaderRegister.vue'
 export { default as LoginColl } from '../../components/LoginColl.vue'
 export { default as LoginUser } from '../../components/LoginUser.vue'
 export { default as Logo } from '../../components/Logo.vue'
@@ -9,6 +10,7 @@ export { default as Main } from '../../components/Main.vue'
 export const LazyFooter = import('../../components/Footer.vue' /* webpackChunkName: "components/Footer'}" */).then(c => c.default || c)
 export const LazyHeader = import('../../components/Header.vue' /* webpackChunkName: "components/Header'}" */).then(c => c.default || c)
 export const LazyHeaderMenu = import('../../components/HeaderMenu.vue' /* webpackChunkName: "components/HeaderMenu'}" */).then(c => c.default || c)
+export const LazyHeaderRegister = import('../../components/HeaderRegister.vue' /* webpackChunkName: "components/HeaderRegister'}" */).then(c => c.default || c)
 export const LazyLoginColl = import('../../components/LoginColl.vue' /* webpackChunkName: "components/LoginColl'}" */).then(c => c.default || c)
 export const LazyLoginUser = import('../../components/LoginUser.vue' /* webpackChunkName: "components/LoginUser'}" */).then(c => c.default || c)
 export const LazyLogo = import('../../components/Logo.vue' /* webpackChunkName: "components/Logo'}" */).then(c => c.default || c)
--- Pets_Care_Nuxt/.nuxt/router.js
+++ Pets_Care_Nuxt/.nuxt/router.js
@@ -6,6 +6,8 @@ import scrollBehavior from './router.scrollBehavior.js'
 const _42aefaf7 = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
 const _5558ed8a = () => interopDefault(import('../pages/contact.vue' /* webpackChunkName: "pages/contact" */))
 const _2c0e78d3 = () => interopDefault(import('../pages/login.vue' /* webpackChunkName: "pages/login" */))
+const _f782d096 = () => interopDefault(import('../pages/registerColl.vue' /* webpackChunkName: "pages/registerColl" */))
+const _cbe165d8 = () => interopDefault(import('../pages/registerUser.vue' /* webpackChunkName: "pages/registerUser" */))
 const _6266d5bc = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
@@ -36,6 +38,14 @@ export const routerOptions = {
     path: "/login",
     component: _2c0e78d3,
     name: "login"
+  }, {
+    path: "/registerColl",
+    component: _f782d096,
+    name: "registerColl"
+  }, {
+    path: "/registerUser",
+    component: _cbe165d8,
+    name: "registerUser"
   }, {
     path: "/",
     component: _6266d5bc,
